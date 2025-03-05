9592228b636d74b4750ba27e87c1d6aece556621
Mikko Palarca <127636270+xmk1@users.noreply.github.com>
2025-03-05 12:06:23 +0800

    Update palarca.py

--- palarca.py
+++ palarca.py
@@ -42,17 +42,9 @@ class Interpreter(object):
         self.current_token = None
         self.current_char = self.text[self.pos]
     
-    def skip_whitespace(self):
-        while self.current_char is not None and self.current_char.isspace():
-                self.advance()
+    def error(self):
+        raise Exception('Error parsing input')
                 
-    def integer(self):
-        """Return a (multidigit) integer consumed from the input. """
-        result = ''
-        while self.current_char is not None and self.current_char.isdigit():
-            result += self.current_char
-            self.advance()
-        return int(result)
         
     def advance(self):
         """Advance the 'pos' pointer and set the 'current_char' variable."""
@@ -61,9 +53,18 @@ class Interpreter(object):
             self.current_char = None # Indicates end of input
         else:
             self.current_char = self.text[self.pos]
+            
+    def skip_whitespace(self):
+        while self.current_char is not None and self.current_char.isspace():
+                self.advance()
 
-    def error(self):
-        raise Exception('Error parsing input')
+    def integer(self):
+        """Return a (multidigit) integer consumed from the input. """
+        result = ''
+        while self.current_char is not None and self.current_char.isdigit():
+            result += self.current_char
+            self.advance()
+        return int(result)
 
     def get_next_token(self):
         """ Lexical analyzer (also known as scanner or tokenizer)
@@ -100,6 +101,13 @@ class Interpreter(object):
             self.current_token = self.get_next_token()
         else: 
             self.error()
+            
+            
+    def term(self):
+        """Return an INTEGER token value."""
+        token = self.current_token
+        self.eat(INTEGER)
+        return token.value
 
     def expr(self):
         """ expr -> INTEGER PLUS INTEGER """
@@ -107,35 +115,17 @@ class Interpreter(object):
         # set current token to the first token taken from the input
         self.current_token = self.get_next_token()
 
-        # we expect the current token to be a single-digit integer
-        left = self.current_token
-        self.eat(INTEGER)
-
-        # we expect the current token to be a '+' token
-        op = self.current_token
-        if op.type == PLUS:
-            self.eat(PLUS)
-        else:
-            self.eat(MINUS)
-
-        # we expect the current token to be as single-digit integer
-        right = self.current_token
-        self.eat(INTEGER)
-
-        # after the above call the self.current_token is set to
-        # EOF token
-
-        # at this point INTEGER PLUS INTEGER or the INTEGER MINUS INTEGER sequence of tokens
-        # has been successfully found and the method can just
-        # return the result of adding or subtracting two integers, thus
-        # effectively interpreting client input
-        if op.type == PLUS:
-            result = left.value + right.value
-        else:
-            result = left.value - right.value
+        result = self.term()
+        while self.current_token.type in (PLUS, MINUS):
+            token = self.current_token
+            if token.type == PLUS:
+                self.eat(PLUS)
+                result = result + self.term()
+            elif token.type == MINUS:
+                self.eat(MINUS)
+                result = result - self.term()
+                
         return result
-        
-    
 
     
 def main():
@@ -157,3 +147,5 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+Previous
